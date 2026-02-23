6576ae708e5e0602d14bc1006f267b74884c67ca
ayazkhan1410 <programmingwithayaz@gmail.com>
2026-02-23 23:08:39 +0500

    Fix middleware code

--- students/middleware.py
+++ students/middleware.py
@@ -1,17 +1,13 @@
 from django.utils.deprecation import MiddlewareMixin
-from django.urls import resolve, Resolver404
 from django.http import JsonResponse
 
 
 class Custom404MessageDisplayMiddleWare(MiddlewareMixin):
-    def process_request(self, request):
-        try:
-            resolve(request.path_info)
-        except Resolver404:
+    def process_response(self, request, response):
+        if response.status_code == 404:
             return JsonResponse({
                 'error': 'Page Not Found',
                 'reason': 'The requested URL does not exist.',
                 'path': request.path,
             }, status=404)
-
-        return None
+        return response
