73a1c49be4c975941d1f7b3217dc5672718cbebe
ayazkhan1410 <programmingwithayaz@gmail.com>
2026-06-20 18:03:19 +0500

    Add vercel link into allowed host

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
+    "https://lesa-academy-portal.vercel.app",
 ]
 _extra_cors = os.getenv('CORS_ALLOWED_ORIGINS', '')
 if _extra_cors:
